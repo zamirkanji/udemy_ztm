bf782b8bffb306f43e3abdf6e93534ebb29f256b
Zamir Kanji <zamirkanji@comcast.net>
2020-02-03 15:49:10 -0800

    update 2.3.20

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,49 @@
-// let firstNumber = prompt("Enter your first number");
-// let secondNumber = prompt("Enter your second number");
+// // let firstNumber = prompt("Enter your first number");
+// // let secondNumber = prompt("Enter your second number");
 
-// let sum = Number(firstNumber) + Number(secondNumber);
-// let sub = Number(firstNumber) - Number(secondNumber);
-// let mult = Number(firstNumber) * Number(secondNumber);
-// let div = Number(firstNumber) / Number(secondNumber);
+// // let sum = Number(firstNumber) + Number(secondNumber);
+// // let sub = Number(firstNumber) - Number(secondNumber);
+// // let mult = Number(firstNumber) * Number(secondNumber);
+// // let div = Number(firstNumber) / Number(secondNumber);
 
-// alert("The sum is: " + sum + "The subtraction is: " + sub + "The multiplication is: " + mult + "The division is: " + div);
-
-
-
-function callAlert() {
-  console.log("wyd bro!");
-}
-callAlert();
-
-let sayBye = function () { //function expression (anonymous function)
-  console.log("bye bro bro!");
-}
-sayBye();
-
-//arguments (basic
-function sing(song) {
-  console.log(song);
-}
-sing("doe re mi!");
+// // alert("The sum is: " + sum + "The subtraction is: " + sub + "The multiplication is: " + mult + "The division is: " + div);
 
 
-//return 
-let a = function multiply(a, b) {
-  if (a > 10 || b > 10) {
-    return "that\'s too hard for my small brain!";
-  } else {
-    return a * b; //return exits function (final way to end function)
-  }
-  return a * b;
-}
-// multiply();
-// a();
-console.log(a(4, 5));
-alert(a(12, 12)); //that's too hard for my small brain as an alert
 
-let b = a; //changing variables
+// function callAlert() {
+//   console.log("wyd bro!");
+// }
+// callAlert();
 
-//  parameters - are the actual arguments (a,b)
-//  arguments - function calls arguments 
+// let sayBye = function () { //function expression (anonymous function)
+//   console.log("bye bro bro!");
+// }
+// sayBye();
+
+// //arguments (basic
+// function sing(song) {
+//   console.log(song);
+// }
+// sing("doe re mi!");
+
+
+// //return 
+// let a = function multiply(a, b) {
+//   if (a > 10 || b > 10) {
+//     return "that\'s too hard for my small brain!";
+//   } else {
+//     return a * b; //return exits function (final way to end function)
+//   }
+//   return a * b;
+// }
+// // multiply();
+// // a();
+// console.log(a(4, 5));
+// alert(a(12, 12)); //that's too hard for my small brain as an alert
+
+// let b = a; //changing variables
+
+// //  parameters - are the actual arguments (a,b)
+// //  arguments - function calls arguments 
+
+// //ARRAYS/OBJECTS
---
+++ javascript/script2.js
@@ -0,0 +1,21 @@
+let user = {
+  name: "Zamir",
+  age: 25,
+  hobby: "Snowboarding",
+  isMarried: false,
+  spells: ["abrakadabra", "shazam", "boo"],
+  shout: function () {
+    console.log("AHHHH!");
+  }
+};
+
+let list = [
+  {
+    username: "Andy",
+    password: "secret"
+  },
+  {
+    username: "Jeff",
+    password: "123"
+  }
+];
